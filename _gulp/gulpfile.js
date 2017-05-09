19bdd6d7cdf78758a2a2bcf3aa36bf56a545b1bd
Your Name <you@example.com>
2017-05-09 15:59:14 +0900

    working

---
+++ _gulp/gulpfile.js
@@ -0,0 +1,113 @@
+var gulp = require('gulp');
+
+// 汎用プラグイン
+var rename  = require('gulp-rename');
+var plumber = require('gulp-plumber');
+var notify  = require('gulp-notify');
+var watch   = require('gulp-watch');
+var del     = require('del');
+
+// css関係
+var sass         = require('gulp-ruby-sass');
+var csscomb      = require('gulp-csscomb');
+var autoprefixer = require('gulp-autoprefixer');
+
+// ブラウザ関係
+var browserSync = require('browser-sync').create();
+
+// 画像関係
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
+
+
+var paths = {
+  sass: './src/**/*.scss',
+  png: './src/**/*.png',
+  cp: [
+    './src/**/*',
+    '!./src/**/{*.scss,*.png}'
+  ],
+  dest: '../'
+};
+
+
+gulp.task('sass', function() {
+  return sass('src/', {
+    loadPath: './sass_imports',
+    style: 'nested'
+  })
+  .pipe(autoprefixer({
+    browsers: ['android 2.3'],
+    remove: false
+  }))
+  .pipe(csscomb())
+  .pipe(gulp.dest(paths.dest))
+  .pipe(browserSync.stream());
+});
+
+gulp.task('img', function(){
+
+  /*
+  pngquantでqualityの範囲外の画像は圧縮されずdistディレクトリにも
+  出力されないので、先に画像を全てコピーしておく
+  また、画質に関係しないデータは削除しておきたいためimageminをかけておく
+  */
+  gulp.src(paths.png)
+      .pipe(imagemin())
+      .pipe(gulp.dest(paths.dest))
+      .on('end', function() {
+        gulp.src(paths.png)
+            .pipe(imagemin({
+              progressive: true,
+              use: [
+                pngquant({
+                  quality: '60-80'
+                })
+              ]
+            }))
+            /*
+            pngquantはガンマ補正情報を埋め込む場合があり、それが残っているとpng画像だけ
+            周りと色の差が発生してしまう場合があるので、再度imageminをかけて補正情報を除去
+            */
+            .pipe(imagemin())
+            .pipe(gulp.dest(paths.dest));
+      });
+});
+
+gulp.task('cp', function() {
+  gulp.src(paths.cp)
+      .pipe(gulp.dest(paths.dest));
+});
+
+// gulp.task('clean', function(cb) {
+//   del([
+//     paths.dest + '/**/*',
+//     '!' + paths.dest  + '/.git'
+//   ]).then(function() {
+//     cb();
+//   });
+// });
+//
+gulp.task('build', function() {
+  gulp.start(['cp', 'sass', 'img']);
+});
+
+gulp.task('default', ['build'], function() {
+  browserSync.init({
+    // 案件に応じて、proxyかserverどちらかの行を有効に
+    // proxy: "192.168.11.5:8000",
+    server: paths.dest,
+    open: false,
+    ghostMode: false
+  });
+
+  watch('./src/**/*.scss', function(event) {
+    gulp.start('sass');
+  });
+  watch([paths.png], function(event) {
+    gulp.start('img');
+  });
+  watch(paths.cp, function(event) {
+    gulp.start('cp');
+  });
+});
---
+++ _gulp/src/js/main.js
@@ -0,0 +1,163 @@
+;(function() {
+  var PagerComponent = {
+    props: ['current', 'total'],
+    template: '#pager-component-template',
+    computed: {
+      prevIsExist: function() {
+        return (this.current > 0);
+      },
+      nextIsExist: function() {
+        return (this.current+2 < this.total);
+      },
+      prevUrl: function() {
+        return '/page/' + (this.current - 1);
+      },
+      nextUrl: function() {
+        return '/page/' + (this.current + 1);
+      }
+    },
+    methods: {
+      showPrev: function() {
+        this.$emit('prev');
+      },
+      showNext: function() {
+        this.$emit('next');
+      }
+    }
+  };
+  var IndexComponent = {
+    data: function() {
+      return {
+        currentPage: 0,
+        count: 10
+      };
+    },
+    props: ['articles'],
+    template: '#index-component-template',
+    computed: {
+      dispArticles: function() {
+        return this.articles.slice(this.count*this.currentPage, this.count*(this.currentPage+1));
+      },
+      totalPage: function() {
+        return (this.articles.length / this.count) + 1;
+      }
+    },
+    methods: {
+      prevPage: function() {
+        this.currentPage--;
+      },
+      nextPage: function() {
+        this.currentPage++;
+      }
+    },
+    watch: {
+      '$route': function(to, from) {
+        if (typeof to.params.index === 'undefined') {
+          this.currentPage = 0;
+        } else {
+          this.currentPage = parseInt(to.params.index);
+        }
+      }
+    },
+    components: {
+      'link-component': {
+        props: ['article'],
+        template: '<router-link :to="article.url">{{ article.title }}</router-link>',
+        computed: {
+          url: function() {
+            return '/posts/';
+          }
+        }
+      },
+      'pager-component': PagerComponent
+    },
+    mounted: function() {
+      document.title = 'index';
+      if (typeof this.$route.params.index === 'undefined') {
+        this.currentPage = 0;
+      } else {
+        this.currentPage = parseInt(this.$route.params.index);
+      }
+    }
+  };
+  var PostsComponent = {
+    data: function() {
+      return {
+        content: ''
+      };
+    },
+    template: '#posts-component-template',
+    computed: {
+      date: function() {
+        return (this.$route.params.perma_link.split('__'))[0];
+      },
+      title: function() {
+        return (this.$route.params.perma_link.split('__'))[1];
+      }
+    },
+    mounted: function() {
+      document.title = this.title;
+
+      var self = this;
+      $.ajax({
+        type: 'GET',
+        url: '/posts/' + this.$route.params.perma_link + '/README.md',
+      })
+      .then(
+        function(res) {
+          self.content = marked(res);
+        },
+        function() {
+          console.log('error');
+        }
+      );
+    }
+  };
+  var NotFoundComponent = {
+    template: '<div>not found.</div>',
+    mounted: function() {
+      document.title = 'not found';
+    }
+  };
+
+  var app = new Vue({
+    data: {
+      articles: []
+    },
+    el: '#app',
+    router: new VueRouter({
+      // mode: 'history',
+      routes: [
+        { path: '/', component: IndexComponent },
+        { path: '/page/:index/', component: IndexComponent },
+        { path: '/posts/:perma_link/', component: PostsComponent },
+        { path: '*', component: NotFoundComponent }
+      ]
+    }),
+    created: function() {
+      var self = this;
+      $.ajax({
+        type: 'GET',
+        url: 'https://api.github.com/repos/ryou/ryou.github.io/contents/posts',
+        dataType: 'jsonp',
+        jsonpCallback: 'hoge'
+      })
+      .then(
+        function(json) {
+          json.data.forEach(function(e, i, a) {
+            var tmp = e.name.split('__');
+            var obj = {
+              url: '/posts/' + e.name + '/',
+              date: tmp[0],
+              title: tmp[1]
+            };
+            self.articles.push(obj);
+          });
+        },
+        function() {
+          console.log('error');
+        }
+      );
+    }
+  });
+})();
